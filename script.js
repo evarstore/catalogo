d12873f85ab7791e0430d1a3373c851d94768b9c
evarstore <lokhito3@gmail.com>
2026-03-14 15:09:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function comprar(producto, precio){
 
-const numero = "51931417388";
+const numero = "51960768707";
 
 const mensaje = `Hola, quiero comprar el reloj ${producto} con precio S/${precio}. ¿Está disponible?`;
 
